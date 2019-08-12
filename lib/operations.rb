3eb9e9592c842480fe5ce9ce73b4c58d511f3ee7
Katie Seggelke <katie.seggelke@gmail.com>
2019-08-12 21:51:01 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,8 +1,6 @@
 def unsafe?(speed)
   if speed < 40 || speed > 60
     return true 
-  #elsif speed > 60
-   # return true
   else 
     return false
   end 
@@ -10,8 +8,9 @@ end
 
 def not_safe?(speed)
 	#speed < 40 && speed > 60 ? true : false
-	speed > 60 ? true : false 
-	speed < 40 ? true : false 
+	#speed > 60 ? true : false 
+	#speed < 40 ? true : false 
+	speed < 40 || speed > 60 ? true : false
 end
 	
 
